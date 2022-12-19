bc68135c171445b0b2da33f07e0687db89663533
tgoff765 <85459537+tgoff765@users.noreply.github.com>
2022-12-19 10:44:28 -0500

    Update aldousbroder_demo.py

--- demos/aldousbroder_demo.py
+++ demos/aldousbroder_demo.py
@@ -2,5 +2,5 @@ from maze_creator.core.maze import Maze, MazeType
 
 if __name__ == "__main__":
     # Can create a maze a nd print ASCII representation
-    maze = Maze(MazeType.ALDOUSBRODER, columns=100, rows=100)
+    maze = Maze(MazeType.ALDOUSBRODER, columns=100,  rows=100)
     maze.draw()
