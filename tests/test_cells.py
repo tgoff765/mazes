627d9b750b0ea849ed15a3135f2c21148adce94e
Tyler Goff <tgoff@Tylers-MacBook-Pro-2.local>
2022-12-19 10:40:04 -0500

    linting changes

--- tests/test_cells.py
+++ tests/test_cells.py
@@ -10,7 +10,7 @@ def test_no_neighbors():
 
 
 def test_link():
-    test_cell = Cell(1,1)
-    test_cell_2 = Cell(2,2)
+    test_cell = Cell(1, 1)
+    test_cell_2 = Cell(2, 2)
     test_cell.link(test_cell_2)
     assert test_cell.is_linked(test_cell_2)
