fef58691824f9b7563f07f27f171cddf30828567
Tyler Goff <tgoff@Tylers-MacBook-Pro-2.local>
2022-12-20 10:07:42 -0500

    Adding more basic tests

---
+++ tests/test_grid.py
@@ -0,0 +1,14 @@
+from maze_creator.core.grid import Grid
+
+
+def test_grid_setup():
+    test_grid = Grid(2, 2)
+    assert test_grid.columns == 2
+    assert test_grid.rows == 2
+
+
+def test_grid_size():
+    test_grid = Grid(2, 5)
+    assert test_grid.size() == 10
+
+
